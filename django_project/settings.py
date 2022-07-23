b3112c9e464784980d5dffe6dd8bd0b8ede705a0
Lakshmi Narasimhan <badri.dilbert@gmail.com>
2022-07-23 08:58:02 +0530

    Add allowed hosts.

--- django_project/settings.py
+++ django_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
-ALLOWED_HOSTS = ["mysterious-hollows-62532.herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["localhost", "127.0.0.1"]) 
 
 
 # Application definition
